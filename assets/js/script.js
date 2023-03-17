810d60f40d3c2de74f0043988e5b1b0ad334d1e5
Togothor <121944070+Togothor@users.noreply.github.com>
2023-03-17 22:30:52 +0100

    initial commit
    added basics

---
+++ assets/js/script.js
@@ -0,0 +1,14 @@
+// a function that starts the game 
+
+//a function that creates the playfield
+
+//a function that checks winning order
+
+ 
+//a function that marks clicked square and logs it
+
+
+//a function that resets the game & playfield
+
+
+//a function that sends the results to playerscore
