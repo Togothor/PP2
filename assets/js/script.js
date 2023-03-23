44a4984603e8702b33f757ff7be6f098523ccff2
Togothor <121944070+Togothor@users.noreply.github.com>
2023-03-23 12:23:24 +0100

    add styling for heading and inside container

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,21 +1,31 @@
-// a function that starts the game 
+/**Inside here the whole game *lives*-----------------------------------*/
+const container = document.getElementById("grid-container");       
 
-function startGame () {
 
-}
-//a function that creates the playfield
+  function createBoard() { 
+      // Creates container.
+      const columnContainer = document.createElement("div");
+          columnContainer.id= "columnContainer";
+             container.appendChild(columnContainer);
 
-function createPlayField () {
+      // Creates *3 columns horisontally.
+      for (let i = 0; i < 3; i++) {
+        const column = document.createElement("div");                      
+          column.classList.add("column"); 
+
+      // Creates *3 divs inside the columns vertically.
+        for (let j = 0; j < 3; j++) {
+        const box = document.createElement("div");                       
+          box.classList.add("box");                                        
+            box.setAttribute("id", `${i}-${j}`);                             
+              column.appendChild(box);                                         
+      }
+// Appends the child(column) to its parent (columnContainer)
+columnContainer.appendChild(column);                                     
+
+
+}} 
+
+
+createBoard();  
     
-}
-
-//a function that checks winning order
-
- 
-//a function that marks clicked square and logs it
-
-
-//a function that resets the game & playfield
-
-
-//a function that sends the results to playerscore
